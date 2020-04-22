8b62569940b9f09d9ef95bff24235fec14f8f349
abdifaishal <abdi.faishal@gmail.com>
2020-04-22 13:48:45 +0700

    add starter file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Untuk tombol menu di responsif navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
