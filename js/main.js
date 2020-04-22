c1f268bb751e0973ef3a09d770f6ce9a3f924b2d
abdifaishal <abdi.faishal@gmail.com>
2020-04-22 21:00:51 +0700

    add animation on scroll

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+// Effect fade in
+const teamCard = document.querySelectorAll(".team");
+
+const option = {
+  threshold: 1,
+};
+
+const observerFadeIn = new IntersectionObserver((entries, observerFadeIn) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("animate-top-toggle");
+      observerFadeIn.unobserve(entry.target);
+    }
+  });
+}, option);
+
+teamCard.forEach((card) => {
+  observerFadeIn.observe(card);
+});
